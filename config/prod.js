002a3006595af00db39aba6743914716c3d71e89
Xiaohan Zhang <xiaohan.zhang@arts.usask.ca>
2016-02-04 10:44:01 -0600

    tmp

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,24 @@ module.exports = {
   database: {
     uri: 'mongodb://localhost/tc',
   },
+  auth: {
+    'facebookAuth' : {
+        'clientID'      : '***REMOVED***', // your App ID
+        'clientSecret'  : '***REMOVED***', // your App Secret
+        'callbackURL'   : 'http://206.12.59.55:3000/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : '***REMOVED***',
+        'consumerSecret'    : '***REMOVED***',
+        'callbackURL'       : 'http://206.12.59.55:3000/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '***REMOVED***',
+        'clientSecret'  : '***REMOVED***',
+        'callbackURL'   : 'http://206.12.59.55:3000/auth/google/callback'
+    }
+  },
   port: 80,
 };
